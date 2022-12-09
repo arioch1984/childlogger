a2532afea2d4f34f78f89178ef6a010ef7b07b7b
fabio.brunelli <arioch1984@gmail.com>
2022-12-10 00:58:46 +0100

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="HomePage">
+    <h1>Title</h1>
+    <div class="row">
+      <div class="col-8">pippo</div>
+      <div class="col-4">pluto</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "HomePage"
+}
+
+</script>
+
+<style scoped>
+
+</style>
